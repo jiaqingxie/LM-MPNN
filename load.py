d2cc66f690add2c9abc28b84ffd76d25309b7a25
JiaX-TCS <jiaxie@student.ethz.ch>
2023-12-11 23:38:28 +0100

    add MoleculeNet

--- load.py
+++ load.py
@@ -1,6 +1,6 @@
 import torch
 from tqdm import tqdm
-from torch_geometric.datasets import QM9
+from torch_geometric.datasets import QM9, MoleculeNet
 from torch_geometric.data import InMemoryDataset, Data
 from transformers import AutoTokenizer
 
@@ -30,6 +30,47 @@ class LM_QM9(InMemoryDataset):
 
         data_list = []
         for idx, data in enumerate(qm9):
+            data.x = tokenized_features[idx].unsqueeze(0)
+            data_list.append(data)
+            is_padding = (data.x == 1) 
+            data.attention_mask =  ~is_padding
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+    def pretrain(self, smiles_list, pretrain_model):
+        tokenizer = AutoTokenizer.from_pretrained("seyonec/{}".format(pretrain_model))
+        tokenized_features = tokenizer.batch_encode_plus(smiles_list, padding=self.padding, truncation=True, return_tensors='pt')
+        return tokenized_features['input_ids']
+
+
+### MoleculeNet encoded with tokens
+class LM_MoleculeNet(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pretrain_model = "ChemBERTa-zinc-base-v1", padding = True, name = "ESOL"):
+        self.pretrain_model = pretrain_model
+        self.padding = padding
+        self.name = name
+        super(LM_MoleculeNet, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+        
+    @property
+    def raw_file_names(self):
+        return ['some_file_name']
+
+    @property
+    def processed_file_names(self):
+        return ['lm_molnet.pt']
+
+    def download(self):
+        pass
+
+    def process(self):
+        mol = MoleculeNet("data/{}".format(self.name), self.name)
+        smiles_list = [data.smiles for data in mol]
+        tokenized_features = self.pretrain(smiles_list, self.pretrain_model)
+
+        data_list = []
+        for idx, data in enumerate(mol):
             data.x = tokenized_features[idx].unsqueeze(0)
             data_list.append(data)
             is_padding = (data.x == 1) 
