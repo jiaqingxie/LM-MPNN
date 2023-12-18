4cf23b086aef1d9215f2bd1f01c8b9bdb7386e7e
ZH C <chiziheng15@gmail.com>
2023-12-18 14:37:26 +0100

    added GNN baseline

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import GRU, Linear, ReLU, Sequential
+from torch_geometric.nn import NNConv, Set2Set
+
 class ChemBERTaForPropertyPrediction(nn.Module):
     def __init__(self, chemberta_model):
         super().__init__()
@@ -9,3 +14,32 @@ class ChemBERTaForPropertyPrediction(nn.Module):
         outputs = self.chemberta(input_ids, attention_mask=attention_mask, output_hidden_states=True)
         hidden_states = outputs.hidden_states[-1]
         return self.regressor(hidden_states[:,0]).view(-1)
+
+class NNConvModel(nn.Module):
+    def __init__(self, num_features, dim, graph_embedding_dim):
+        super().__init__()
+        self.lin0 = Linear(num_features, dim)
+
+        nn = Sequential(Linear(4, 128), ReLU(), Linear(128, dim * dim))
+        self.conv = NNConv(dim, dim, nn, aggr='mean')
+        self.gru = GRU(dim, dim)
+
+        self.set2set = Set2Set(dim, processing_steps=3)
+        self.lin1 = torch.nn.Linear(2 * dim, graph_embedding_dim)
+        self.lin2 = torch.nn.Linear(graph_embedding_dim, 1)
+
+    def forward(self, data):
+        out = F.relu(self.lin0(data.x))
+        h = out.unsqueeze(0)
+
+        for i in range(3):
+            m = F.relu(self.conv(out, data.edge_index, data.edge_attr))
+            out, h = self.gru(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+
+        out = self.set2set(out, data.batch)
+        graph_embed = self.lin1(out)
+        out = F.relu(graph_embed)
+        out = self.lin2(out)
+        return out.view(-1), graph_embed
+
